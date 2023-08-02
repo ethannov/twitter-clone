05bc8f91f54df6cf0167e9b704a6fc25f0f87176
ethannov <ethannovicio@gmail.com>
2023-08-01 23:22:59 -0500

    added likes

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers'
 import AuthButtonServer from './auth-button-server';
 import { redirect } from 'next/navigation';
 import NewTweet from './new-tweet';
+import Likes from './likes';
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies })
@@ -13,8 +14,14 @@ export default async function Home() {
     redirect('/login')
   }
 
-  const { data: tweets } = await supabase.from('tweets').
-  select("*, profiles(*)");
+  const { data } = await supabase.from('tweets').
+  select("*, profiles(*), likes(*)");
+
+  const tweets = data?.map(tweet => ({
+    ...tweet,
+    user_has_liked_tweet: tweet.likes.find(like => like.user_id === session.user.id),
+    likes: tweet.likes.length
+  })) ?? [];
 
   return (
     <>
@@ -26,6 +33,7 @@ export default async function Home() {
             {tweet?.profiles?.name} {tweet?.profiles?.username}
           </p>
           <p>{tweet.title}</p>
+          <Likes tweet={tweet} />
         </div>
       ))}
     </>
